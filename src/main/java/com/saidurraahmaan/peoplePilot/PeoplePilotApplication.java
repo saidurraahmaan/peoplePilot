1095e3b0c2af87244c8d712a7dc70048f3c89b9b
saidurraahmaan <saidurraahmaan@gmail.com>
2023-11-18 01:12:38 +0600

    initialized

---
+++ src/main/java/com/saidurraahmaan/peoplePilot/PeoplePilotApplication.java
@@ -0,0 +1,13 @@
+package com.saidurraahmaan.peoplePilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeoplePilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeoplePilotApplication.class, args);
+	}
+
+}
